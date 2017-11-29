8723e999e5a037bafa1a2fd49c59011765ff2951
Willian Yassunaka <willianhy@gmail.com>
2017-11-28 22:48:17 -0200

    adjusting api

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -8,7 +8,7 @@ const creditSchema = new mongoose.Schema({
 
 const debtSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true },
+    value: { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] },
     status: { type: String, required: false, uppercase: true,
         enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
 })
@@ -18,7 +18,7 @@ const billingCycleSchema = new mongoose.Schema({
     month: { type: Number, min: 1, max: 12, required: true},
     year: { type: Number, min: 1970, max: 2100, required: true },
     credits: [creditSchema],
-    debits: [debtSchema]
+    debts: [debtSchema]
 })
 
 module.exports = restful.model('BillingCycle', billingCycleSchema)
