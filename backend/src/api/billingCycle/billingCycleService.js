e248e75668a9d4e422a137f9569790a2349cb771
Willian Yassunaka <willianhy@gmail.com>
2017-12-01 22:39:50 -0200

    handling message errors

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,7 +1,9 @@
 const BillingCycle = require('./billingCycle')
+const errorHandler = require('../common/errorHandler')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({ new: true, runValidators: true })
+BillingCycle.after('post', errorHandler).after('put', errorHandler)
 
 BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count((error, value) => {
@@ -15,9 +17,9 @@ BillingCycle.route('count', (req, res, next) => {
 
 BillingCycle.route('summary', (req, res, next) => {
     BillingCycle.aggregate({
-        $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "debts.value"}}
+        $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}}
     }, {
-        $group: {_id: null, credit: {$sum: "credit"}, debt: {$sum: "$debt"}}
+        $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
     }, {
         $project: {_id: 0, credit: 1, debt: 1}
     }, (error, result) => {
