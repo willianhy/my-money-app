d1a3a6852cb1bd28a87139f37cebfd110bc56097
Willian Yassunaka <willianhy@gmail.com>
2017-12-04 22:07:37 -0200

    index & app components

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
         loaders: [{
             test: /.js[x]?$/,
             loader: 'babel-loader',
-            exclude: /node_modules,
+            exclude: /node_modules/,
             query: {
                 presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
