75348194a09636f980f6d1d7bf642a90d4b1f4a5
Willian Yassunaka <willianhy@gmail.com>
2017-12-06 19:25:50 -0200

    sidebar

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,9 +2,11 @@ import '../common/template/dependecies'
 import React from 'react'
 
 import Header from '../common/template/header'
+import SideBar from '../common/template/sideBar'
 
 export default props => (
     <div className='wrapper'>
         <Header />
+        <SideBar />
     </div>
 )
