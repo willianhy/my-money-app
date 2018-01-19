7e98520fe3ba59d2842b07e3fce46273328f0a02
Willian Yassunaka <willianhy@gmail.com>
2018-01-18 22:06:54 -0200

    adding status field in itemList

--- frontend/src/billingCycle/itemList.jsx
+++ frontend/src/billingCycle/itemList.jsx
@@ -4,6 +4,7 @@ import { bindActionCreators } from 'redux'
 import { Field, arrayInsert, arrayRemove } from 'redux-form'
 import Grid from '../common/layout/grid'
 import Input from '../common/form/input'
+import If from '../common/operator/if'
 
 class ItemList extends Component {
 
@@ -28,6 +29,11 @@ class ItemList extends Component {
                 <td><Field name={`${this.props.field}[${index}].value`} component={Input} 
                     placeholder='Informe o valor' readOnly={this.props.readOnly} /></td>
                 <td>
+                <If test={this.props.showStatus}>
+                    <td><Field name={`${this.props.field}[${index}].status`} component={Input} 
+                        placeholder='Status' readOnly={this.props.readOnly} />
+                    </td>
+                </If>
                     <button type='button' className='btn btn-success'
                         onClick={() => this.add(index + 1)}>
                         <i className="fa fa-plus"></i>
@@ -55,6 +61,9 @@ class ItemList extends Component {
                             <tr>
                                 <th>Nome</th>
                                 <th>Valor</th>
+                                <If test={this.props.showStatus}>
+                                    <th>Status</th>
+                                </If>
                                 <th className='table-actions'>Ações</th>
                             </tr>
                         </thead>
